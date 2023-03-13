df86110d7a4bb5fcedff26722f735d4d731961b2
Charles Lopes <junior.charles@gmail.com>
2023-03-13 10:01:34 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import br.com.dio.desafio.Curso;
+import br.com.dio.desafio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        Curso curso1 = new Curso();
+        curso1.setTítulo("Curso de Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
 
+        Curso curso2 = new Curso();
+        curso2.setTítulo("Curso de JavaScript");
+        curso2.setDescricao("Descrição curso JavaScript");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTítulo("Mentoria de Java");
+        mentoria.setDescricao("Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
 
 }
